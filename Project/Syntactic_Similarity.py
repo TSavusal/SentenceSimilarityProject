092bf994e6017f51423ed14a444e6ed83162fd1b
msavusal <markus.savusalo@windowslive.com>
2018-12-05 14:14:12 +0200

    running out of battery

--- Project/Syntactic_Similarity.py
+++ Project/Syntactic_Similarity.py
@@ -4,22 +4,13 @@ from nltk.tokenize import word_tokenize
 
 def task3SyntacticSim(s1,s2):
 	
+	'''remove punctuation, lowercase, stem'''
 	nltk.download('punkt') # if necessary
 	stemmer = nltk.stem.porter.PorterStemmer()
 	remove_punctuation_map = dict((ord(char), None) for char in string.punctuation)
-	stemed_tokens = stem_tokens()
-	
-def stem_tokens(tokens):
-    return [stemmer.stem(item) for item in tokens]
-
-'''remove punctuation, lowercase, stem'''
-def normalize(text):
-    return stem_tokens(nltk.word_tokenize(text.lower().translate(remove_punctuation_map)))
-
+	stem_tokens = nltk.word_tokenize(text.lower().translate(remove_punctuation_map))
 	vectorizer = TfidfVectorizer(tokenizer=normalize, stop_words='english')
-
-def cosine_sim(text1, text2):
-    tfidf = vectorizer.fit_transform([text1, text2])
+	tfidf = vectorizer.fit_transform([s1, s2])
     return ((tfidf * tfidf.T).A)[0,1]
 	
 def intro():

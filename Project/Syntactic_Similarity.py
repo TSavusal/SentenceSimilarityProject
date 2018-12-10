c982eb918f9580c803e5233e998f8835ac2dc308
msavusal <markus.savusalo@windowslive.com>
2018-12-10 20:14:04 +0200

    Progress on Syntactic

--- Project/Syntactic_Similarity.py
+++ Project/Syntactic_Similarity.py
@@ -1,25 +1,33 @@
-import nltk, string, sklearn
+import nltk
+import string
+import sklearn
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.tokenize import word_tokenize
 
 def task3SyntacticSim(s1,s2):
-<<<<<<< HEAD
+	"""<<<<<<< HEAD
 
 =======
->>>>>>> refs/remotes/origin/master
-	'''remove punctuation, lowercase, stem'''
-	nltk.download('punkt') # if necessary
+>>>>>>> refs/remotes/origin/master"""
 	stemmer = nltk.stem.porter.PorterStemmer()
 	remove_punctuation_map = dict((ord(char), None) for char in string.punctuation)
-	stem_tokens = nltk.word_tokenize(text.lower().translate(remove_punctuation_map))
 	vectorizer = TfidfVectorizer(tokenizer=normalize, stop_words='english')
-	tfidf = vectorizer.fit_transform([s1, s2])
-	return ((tfidf * tfidf.T).A)[0,1]
+
+def stem_tokens(tokens):
+    return [stemmer.stem(item) for item in tokens]
+
+'''remove punctuation, lowercase, stem'''
+def normalize(text):
+    return stem_tokens(nltk.word_tokenize(text.lower().translate(remove_punctuation_map)))
+
+def cosine_sim(s1, s2):
+    tfidf = vectorizer.fit_transform([text1, text2])
+    return ((tfidf * tfidf.T).A)[0,1]
 	
 def intro():
 		print("Syntactic similarity between two sentences\n")
-		sent_one = input("Enter the first sentence to compare similarity : ")
-		sent_two = input("Enter the second sentence to compare similarity : ")
+		sent_one = raw_input("Enter the first sentence to compare similarity : ")
+		sent_two = raw_input("Enter the second sentence to compare similarity : ")
 		print("Calculating...\n")
 		prob_sim_sent = task3SyntacticSim(sent_one, sent_two)
 		print("Distance: ")
@@ -29,8 +37,8 @@ if __name__ == "__main__":
     print("-------------------Syntactic Similarity--------------------------")
     intro()
     print("Want to try once again? If yes enter 1, if not enter 0 : ")
-    againtry = int(input())
+    againtry = int(raw_input())
     while(againtry == 1):
         intro()
         print("Want to try once again? If yes enter 1, if not enter 0 : ")
-        againtry = int(input())
+        againtry = int(raw_input())

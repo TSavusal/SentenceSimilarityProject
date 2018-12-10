749fd8e975f9ce7479907e5c78dd3b614a48edee
msavusal <markus.savusalo@windowslive.com>
2018-12-10 19:19:35 +0200

    bit of work on ngram

--- Project/Ngram_Similarity.py
+++ Project/Ngram_Similarity.py
@@ -1,7 +1,10 @@
-def ngrams(sequence, n, pad_left=False, pad_right=False, pad_symbol=None):
-    """
-    A utility that produces a sequence of ngrams from a sequence of items.
-    """
+import nltk
+
+def task3SyntacticSim(sequence, n, pad_left=False, pad_right=False, pad_symbol=None):
+<<<<<<< HEAD
+
+=======
+>>>>>>> refs/remotes/origin/master
 
     if pad_left:
         sequence = chain((pad_symbol,) * (n-1), sequence)
@@ -10,4 +13,23 @@ def ngrams(sequence, n, pad_left=False, pad_right=False, pad_symbol=None):
     sequence = list(sequence)
 
     count = max(0, len(sequence) - n + 1)
-    return [tuple(sequence[i:i+n]) for i in range(count)] 
+    return [tuple(sequence[i:i+n]) for i in range(count)]
+	
+	def intro():
+		print("Ngram similarity between two sentences\n")
+		sent_one = input("Enter the first sentence to compare similarity : ")
+		sent_two = input("Enter the second sentence to compare similarity : ")
+		print("Calculating...\n")
+		prob_sim_sent = task3NgramSim(sent_one, sent_two)
+		print("Distance: ")
+		print(prob_sim_sent)
+	
+if __name__ == "__main__":  
+    print("-------------------Ngram Similarity--------------------------")
+    intro()
+    print("Want to try once again? If yes enter 1, if not enter 0 : ")
+    againtry = int(input())
+    while(againtry == 1):
+        intro()
+        print("Want to try once again? If yes enter 1, if not enter 0 : ")
+        againtry = int(input())

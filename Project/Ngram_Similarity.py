922ac94226761c8d9435412eced27129c330264e
msavusal <markus.savusalo@windowslive.com>
2018-12-12 13:41:47 +0200

    Ngram returns value

--- Project/Ngram_Similarity.py
+++ Project/Ngram_Similarity.py
@@ -49,7 +49,7 @@ def task3NgramSim(sent1,sent2):
 	a = get_tuples_nosentences(sent1)
 	b = get_tuples_nosentences(sent2)
 	"""print("Ngram distance: {}".format(cosine_similarity_ngrams(a, b)))"""
-	return cosinecosine_similarity_ngrams(a, b)
+	return cosine_similarity_ngrams(a, b)
 	
 def intro():
 	print("Ngram similarity between two sentences\n")
